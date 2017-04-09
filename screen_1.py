829e92c6069ca10c3256b2b2a9b79fd3eb3ef4a1
pkuzhd <pkuzhd@pku.edu.cn>
2017-04-09 23:50:57 +0800

    Update screen_1.py

--- screen_1.py
+++ screen_1.py
@@ -1,4 +1,5 @@
 background_image_filename = 'sushiplate.jpg'
+mouse_image_filename = 'fugu.png'
  
 import pygame
 from pygame.locals import *
@@ -7,7 +8,8 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((640, 480), 0, 32)
 background = pygame.image.load(background_image_filename).convert()
- 
+mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
+
 Fullscreen = False
 print(pygame.display.list_modes())
 while True:
@@ -30,5 +32,11 @@ while True:
         screen.blit(background, (192,144))
     else:
         screen.blit(background, (0,0))
-    
+    x, y = pygame.mouse.get_pos()
+    #获得鼠标位置
+    x-= mouse_cursor.get_width() / 2
+    y-= mouse_cursor.get_height() / 2
+    #计算光标的左上角位置
+    screen.blit(mouse_cursor, (x, y))
+    #把光标画上去
     pygame.display.update()
