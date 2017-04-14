dee457fc6578f6bc4b65c4923b3d9bfdd8a513d8
pkuzhd <pkuzhd@pku.edu.cn>
2017-04-14 16:12:41 +0800

    v1

--- gpio_1.py
+++ gpio_1.py
@@ -1,7 +1,7 @@
 #The first GPIO file
 import RPi.GPIO as GPIO
 import time
- 
+from sakshat import SAKSHAT 
 GPIO.setmode(GPIO.BCM)
  
 DS = 6
@@ -23,29 +23,28 @@ def writeBit(data):
     GPIO.output(SHCP, GPIO.LOW)
     GPIO.output(SHCP, GPIO.HIGH)
  
-#写入8位LED的状态
 def writeByte(data):
     for i in range (0, 8):
         writeBit((data >> i) & 0x01)
-    #状态刷新信号
     GPIO.output(STCP, GPIO.LOW)
     GPIO.output(STCP, GPIO.HIGH)
+
+def main():
+    b = SAKS.buzzer #feng ming qi
+    b.keep(1)
+    alloff = list((
  
 try:
     init()
     while True:
-        #以下一组8个编码由一组二进制转换而成：
         #00000001,00000010,00000100,00001000,00010000,00100000,01000000,10000000
-        #分别对应8个LED点亮状态
-        for i in [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80]:
+        for i in [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF]:
             writeByte(i)
-            time.sleep(0.1)
-        #LED组全开
+           time.sleep(0.1)
         #writeByte(0xff)
         #time.sleep(0.1)
  
 except KeyboardInterrupt:
     print("except")
-    #LED组全关
     writeByte(0x00)
     GPIO.cleanup()
