4adecc6247bc0faeb06ebebb2dedea8efa0199ee
pkuzhd <pkuzhd@pku.edu.cn>
2017-05-24 20:21:19 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,50 +1,84 @@
 # -*- coding: utf-8 -*-
 import pygame
+from pygame.locals import *
 import pyaudio
 import struct
-from pygame.locals import *
 from DXK import *
+#import gpio_1
 import random
+import time
 
-map_list = ((1, 1, 1, 1, 0), (1, 0, 1, 0, 1), (1, 0, 0, 1, 1), (1, 0, 0, 0, 1))
-
+map_list = ((0, 1, 1, 1, 1), (1, 0, 1, 0, 1), (1, 0, 0, 1, 1), (1, 0, 0, 0, 1),(0, 0, 0, 0, 1))
+#随机地图块元组
+screen_size = (800, 600)
+#屏幕大小
 
 def main():
     pygame.init()
-    screen_size = (800, 600)
-    screen = pygame.display.set_mode(screen_size)
+    screen = pygame.display.set_mode((800, 600))
     #初始化界面
+    
+    font = pygame.font.SysFont("arial", 24)
+    #显示文字的字体格式
+
     pa = pyaudio.PyAudio()
     SAMPLING_RATE = int(pa.get_device_info_by_index(0)['defaultSampleRate'])
-    NUM_SAMPLES = 1000
+    NUM_SAMPLES = 4096
     stream = pa.open(format=pyaudio.paInt16, channels=1, rate=SAMPLING_RATE, input=True, frames_per_buffer=NUM_SAMPLES)
     #初始化声音输入
     score = 0
     score_max = 0
-    while True:
-        #游戏总循环
-        dxk = DXK("dxk1.png")
-        block = Block("black.png", 0)
-        map_queue_1 = list()
-        map_queue_2 = list()
-        for i in range(9):
-            map_queue_1.append(1)
 
-        for i in map_list[2]:
-            map_queue_2.append(i)
+    with open("score.txt", "r") as f:
+        lst = f.read().split()
+        if len(lst) == 2:
+            score_max = int(lst[1])
+    start = False
+
+    dxk = DXK("dxk1.png")
+    block = Block("black.png")
+    #角色和方块对象
+    
+    #游戏总循环
+    while True:
+        map_queue_1 = list()
+        #当前地图方块队列
+        map_queue_2 = list()
+        #等待地图方块队列
+        for i in range(9):
+            map_queue_1.append([1, 1])
+        for i in map_list[3]:
+            map_queue_2.append([i, 1])
         #地图初始化
 
-        h = screen_size[1]-dxk.height-100
-        reset = False
-        Fullscreen = False
-        clock = pygame.time.Clock()
-
+        h0 = screen_size[1]-dxk.height-block.height
+        #地面高度
+        h = h0
         block_s = 0.
+
+        reset = False
         fall = False
-        
+    
+        while not start:
+            screen.fill((255, 255, 255))
+            for i in range(9):
+                if map_queue_1[i]!=0:
+                    screen.blit(block.image, (i * 100. + block_s, block.y))
+            screen.blit(dxk.image, (screen_size[0]/2-dxk.width/2, h))
+            screen.blit(font.render("Please speak loudly to start!", True, (0,0,0)),(260,100))
+            string_audio_data = stream.read(NUM_SAMPLES)
+            k = max(struct.unpack('%dh'%(NUM_SAMPLES,), string_audio_data))
+            #gpio_1.light(k*8//32767)
+            if k*8//32767 == 8:
+                start = True
+            pygame.display.update()
+        stream.close()    
+        clock = pygame.time.Clock()
+        stop = False
+
+
         while True:
             #当前游戏循环
-
             for event in pygame.event.get():
                 #事件处理
                 if event.type == QUIT:
@@ -54,97 +88,109 @@ def main():
                         exit()
                     if event.key == K_r:
                         reset = True
-                    if event.key == K_UP:
-                        dxk.jump(2.5)
-                    if event.key == K_SPACE:
-                        dxk.move()
-                    if event.key == K_f:
-                        Fullscreen = not Fullscreen
-                        if Fullscreen:
-                            screen = pygame.display.set_mode(screen_size, FULLSCREEN | HWSURFACE, 32)
-                        else:
-                            screen = pygame.display.set_mode(screen_size, 0, 32)
-                if event.type == KEYUP:
-                    if event.key == K_SPACE:
-                        dxk.stop()
+            #按R键重新开始游戏
             if reset:
                 break
-            if Fullscreen:
-                screen.fill((255, 255, 255))
-            else:
-                screen.fill((255, 255, 255))
+           
+            screen.fill((255, 255, 255))
+            screen.blit(dxk.image, (screen_size[0]/2-dxk.width/2, h))
+            for i in range(9):
+                if map_queue_1[i][0]!=0:
+                    screen.blit(block.image, (i * 100. + block_s, screen_size[1]-block.height))
+            #绘制背景、角色和地图方块
 
+            screen.blit(font.render("Score: %d"%score, True, (0,0,0)),(0, 0))
+            screen.blit(font.render("Maxscore: %d"%score_max, True, (0,0,0)),(0, 20))
+            #绘制分数信息
+
+            stream = pa.open(format=pyaudio.paInt16, channels=1, rate=SAMPLING_RATE, input=True, frames_per_buffer=NUM_SAMPLES)
             string_audio_data = stream.read(NUM_SAMPLES)
-            k = max(struct.unpack('1000h', string_audio_data))
-            print(k)
+            k = max(struct.unpack('%dh' % (NUM_SAMPLES,), string_audio_data))
+            stream.close()
+            light_num = k*8//32767
+            #声音输入
+            #gpio_1.light(light_num)
+            #GPIO显示音量大小
             
-
-
-            if k <= 2000:
+                        
+            if k <= 1500:
                 dxk.stop()
-            else:
+            elif not stop:
                 dxk.move()
                 if not fall and k > 8000:
                     dxk.jump(2.5)
                     if k > 20000:
                         dxk.fly()
+                        
             time_passed = clock.tick()
             time_passed_seconds = time_passed / 1000.0
             t = time_passed_seconds * 100
+            if not dxk.can_jump and not fall:
+                h -= dxk.speed_y * t
+                dxk.speed_y -= dxk.g * t
+                if h >= h0:
+                    dxk.land()
+                    dxk.can_jump = True
+                    h = h0
+            block_s += dxk.speed_x * t
+            #实现角色移动
+            
             if fall:
-                if map_queue_1[5]==1 and block_s < -78:
+                if map_queue_1[5][0]==1 and block_s <= -78:
                     dxk.stop()
+                    stop = True
                 dxk.speed_y = -2.5
                 if h < 600-53:
                     h -= dxk.speed_y * t
                 else:
                     dxk.stop()
+                    break
+            #实现角色掉落
+            if map_queue_1[3][0] == 0 and map_queue_1[3][1]:
+                score += 1
+                if score>score_max:
+                    score_max = score
+                map_queue_1[3][1] = 0
+            f = open("score.txt", 'w')
+            f.write("%d %d"%(score, score_max))
+            f.close()
+            #实现记录分数，用文件输出
 
-            
-            
-            if not dxk.can_jump and not fall:
-                h -= dxk.speed_y * t
-                dxk.speed_y -= dxk.g * t
-                if h >= screen_size[1]-dxk.height-100 - 0.000001:
-                        dxk.land()
-                        dxk.can_jump = True
-                        h = screen_size[1]-dxk.height-100
-            if h >= screen_size[1]-dxk.height-100 - 0.000001:
-                
-                if map_queue_1[4]==0: 
+            if h >= h0:
+                if map_queue_1[4][0]==0: 
                     if block_s < -22 and block_s > -78:
                         fall = True
-                    elif map_queue_1[5]==0 and block_s <=-78:
+                    elif map_queue_1[5][0]==0 and block_s <=-78:
                         fall = True
-                    elif map_queue_1[3]==0 and block_s > -22:
+                    elif map_queue_1[3][0]==0 and block_s > -22:
                         fall = True
-            screen.blit(dxk.image, (screen_size[0]/2-dxk.width/2, h))
+            #掉落判断
+            
 
-            block_s += dxk.speed_x * t / 2
             if block_s <= -100.:
                 map_queue_1.pop(0)
                 map_queue_1.append(map_queue_2.pop(0))
                 block_s += 100.
-            for i in range(9):
-                if map_queue_1[i]!=0:
-                    screen.blit(block.image, (i * 100. + block_s, block.y))
-
-            # if block_s>=-1.:
-            #     print('\t',block_s)
-            #     if map_queue_1[3] == 0:
-            #         score += 1
-            #         print(score)
-            
             while len(map_queue_2) < 2:
-                rand = random.randrange(6) % 3
+                rand = random.randrange(10) % 5
                 for i in map_list[rand]:
-                    map_queue_2.append(i)
+                    map_queue_2.append([i, 1])
+            #地图更新
             pygame.display.update()
 
-        # if score>score_max:
-        #     score_max = score
-        # print(score, score_max)
-        # score = 0
+        while not reset:
+            screen.blit(font.render("Please speak loudly to restart!", True, (0,0,0)),(250,100))
+            stream = pa.open(format=pyaudio.paInt16, channels=1, rate=SAMPLING_RATE, input=True, frames_per_buffer=NUM_SAMPLES)
+            
+            string_audio_data = stream.read(NUM_SAMPLES)
+            k = max(struct.unpack('%dh'%(NUM_SAMPLES,), string_audio_data))
+            stream.close()
+            #gpio_1.light(k*8//32767)
+            if k*8//32767 == 8:
+                break
+            pygame.display.update()
+        #掉落界面
+        score = 0
 
 if __name__ == '__main__':
     main()
